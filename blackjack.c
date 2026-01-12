337e8d5346a6d178b9726e0f3f11e0854b6dda44
Michelle Chen <158612829+MichelleChen2331@users.noreply.github.com>
2026-01-12 13:06:39 -0500

    Fix index increment in deck initialization

--- blackjack.c
+++ blackjack.c
@@ -12,7 +12,7 @@ void init_deck(struct card *deck){
         for (int v = 1; v <= 13; v++){
             deck[index].suit = suits[s];
             deck[index].value = (v>10) ? 10 : v;
-            index ++;
+            index++;
         }
     }
 }
